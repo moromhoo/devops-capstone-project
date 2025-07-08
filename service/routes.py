9c1a5c46857750ce03d972c839aeb1d3b3d1a9ac
Felipe <65565641+moromhoo@users.noreply.github.com>
2025-07-08 12:58:21 -0400

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -87,7 +87,7 @@ def get_accounts(account_id):
     app.logger.info("Request to read an Account with id: %s", account_id)
     account = Account.find(account_id)
     if not account:
-        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+    abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
     return account.serialize(), status.HTTP_200_OK
 
 
@@ -95,17 +95,19 @@ def get_accounts(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-@app.route("/accounts", methods=["GET"])
-def list_accounts():
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_accounts(account_id):
     """
-    List all Accounts
-    This endpoint will list all Accounts
+    Update an Account
+    This endpoint will update an Account based on the posted data
     """
-    app.logger.info("Request to list Accounts")
-    accounts = Account.all()
-    account_list = [account.serialize() for account in accounts]
-    app.logger.info("Returning [%s] accounts", len(account_list))
-    return jsonify(account_list), status.HTTP_200_OK
+    app.logger.info("Request to update an Account with id: %s", account_id)
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id [{account_id}] could not be found.")
+    account.deserialize(request.get_json())
+    account.update()
+    return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
